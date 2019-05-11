ec470fe990d1563bb88561dc54f9a2d1bdd6f42d
Kamzs <kamil.ambroziak.priv@gmail.com>
2019-05-11 14:03:24 +0200

    start project

---
+++ src/main/java/kams/kamspoject/KamspojectApplication.java
@@ -0,0 +1,13 @@
+package kams.kamspoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamspojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamspojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kams/kamspoject/KamspojectApplicationTests.java
@@ -0,0 +1,16 @@
+package kams.kamspoject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KamspojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
